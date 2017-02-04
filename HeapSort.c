4af2d2926dea6163e70c09082a3a14d2c5f5c0e4
Amit Bansal <amitbansal@Amit.local>
2017-02-04 23:58:46 +0530

    updates

--- HeapSort.c
+++ HeapSort.c
@@ -1,8 +1,44 @@
 #include "stdio.h"
 
+void swap(int *a,int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void MaxHeapify(int A[],int n,int i)
+{
+	int l = (2*i)+1;
+	int r = (2*i)+2;
+	int smallest = i;
+
+	if(l<n && A[l] > A[smallest])
+		smallest = l;
+
+	if(r<n && A[r] > A[smallest])
+		smallest = r;
+
+	if(smallest!=i)
+	{
+		swap(&A[i],&A[smallest]);
+		MaxHeapify(A,n,smallest);
+	}
+}
 
 void sort(int A[],int n)
 {
+	int i;
+	for(i=n/2-1;i>=0;i--)
+		MaxHeapify(A,n,i);
+
+	for(i=n-1;i>=0;i--)
+	{
+		swap(&A[0], &A[i]);
+		MaxHeapify(A,i,0);
+	}
 	
 }
 
