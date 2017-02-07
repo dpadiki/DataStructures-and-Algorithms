3b9879aa726ac636ce1d7d08b9374e6c677bf3d0
Amit Bansal <amitbansal@Amit.local>
2017-02-07 23:59:12 +0530

    Updates

--- HeapSort.c
+++ HeapSort.c
@@ -2,55 +2,53 @@
 
 void swap(int *a,int *b)
 {
-	int temp;
-
+	int *temp;
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-void MaxHeapify(int A[],int n,int i)
+void Maxheapify(int A[],int n,int i)
 {
 	int l = (2*i)+1;
 	int r = (2*i)+2;
-	int smallest = i;
+	int largest = i;
 
-	if(l<n && A[l] > A[smallest])
-		smallest = l;
+	if(l<n && A[l] > A[largest])
+		largest = l;
 
-	if(r<n && A[r] > A[smallest])
-		smallest = r;
+	if(r<n && A[r] > A[largest])
+		largest = r;
 
-	if(smallest!=i)
+	if(largest!=i)
 	{
-		swap(&A[i],&A[smallest]);
-		MaxHeapify(A,n,smallest);
+		swap(&A[i],&A[largest]);
+		Maxheapify(A,n,largest);
 	}
 }
-
-void sort(int A[],int n)
+void Heapsort(int A[],int n)
 {
 	int i;
+
 	for(i=n/2-1;i>=0;i--)
-		MaxHeapify(A,n,i);
+		Maxheapify(A,n,i);
 
 	for(i=n-1;i>=0;i--)
 	{
-		swap(&A[0], &A[i]);
-		MaxHeapify(A,i,0);
+		swap(&A[0],&A[i]);
+		Maxheapify(A,i,0);
 	}
-	
 }
-
 int main()
 {
-	int A[] = {7,4,8,5,6,9,3,2,1};
-	int n,i;
+	int A[] = {7,4,1,5,6,3,2};
+	int n = sizeof(A)/sizeof(A[0]);
+	int i;
 
-	n = sizeof(A) / sizeof(A[0]);
-
-	sort(A,n);
+	Heapsort(A,n);
 
 	for(i=0;i<n;i++)
 		printf("%d ",A[i]);
+	
+	return 0;
 }
