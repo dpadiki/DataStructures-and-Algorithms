15ae070a4724aefa02910855cf76adbbca0e6877
Amit Bansal <AmitBansal@Amits-MacBook-Pro.local>
2017-02-17 17:57:35 +0530

    few changes

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,79 +1,215 @@
-#include "stdio.h"
-#include "stdlib.h"
-#define true 1
-#define false 0
+#include <stdio.h>
+#include <stdlib.h>
 
-struct bstNode
+struct Node
 {
+	struct Node* left;
 	int data;
-	struct BstNode* left;
-	struct BstNode* right;
+	struct Node* right;
 };
 
-struct bstNode* Newnode(int n)
+struct Queue
 {
-	struct bstNode* temp;
+	struct Node* data;
+	struct Queue* next;
+};
 
-	temp = (struct bstNode*)malloc(sizeof(struct bstNode));
+struct Queue* front = NULL;
+struct Queue* rear = NULL;
 
-	temp->data = n;
-	temp->right = temp->left = NULL;
+int isEmpty()
+{
+	if(front==NULL)
+		return 1;
+	else
+		return 0;
+}
+
+void Pop()
+{
+	if(isEmpty())
+		return;
+
+	struct Queue* temp = front;
+	front = front->next;
+	free(temp);
+
+	
+}
+
+struct Node* Pfront()
+{
+	struct Node* temp ;
+	temp = front->data;
+	return temp;
+}
+
+void Enqeue(struct Node* data)
+{
+	struct Queue* temp;
+	temp = (struct Queue*)malloc(sizeof(struct Queue));
+	temp->data = data;
+	temp->next=NULL;
+
+	if(front==NULL && rear==NULL)
+	{
+		front = rear = temp;
+		return;
+	}
+
+	rear->next = temp;
+	rear =temp;
+}
+
+struct Node* NewNode(int data)
+{
+	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+	temp->left = NULL;
+	temp->data = data;
+	temp->right = NULL;
 
 	return temp;
 }
 
-int search(struct bstNode* root , int data)
+void PostOrder(struct Node* root)
 {
-	if(root==NULL)  
-		return false;
-	
-	else if(root->data == data)  
-		return true;
+	if(root==NULL)
+		return;
 
-	else if(data <= root->data)
-		return search(root->left,data);
-
-	else
-		return search(root->right,data);
+	PostOrder(root->left);
+	PostOrder(root->right);
+	printf("%d ",root->data);
 }
 
-struct bstNode* insert(struct bstNode* root ,int n)
+struct Node* FindMin(struct Node* root)
 {
-	if(root==NULL){
-		root = Newnode(n);
-	}
-
-	else if(n <= root->data)
-		root->left = insert(root->left,n);
-
-	else 
-		root->right = insert(root->right,n);
+	while(root->left!=NULL)
+		root = root->left;
 
 	return root;
 }
 
+void LevelOrder(struct Node* root)
+{
+	if(root==NULL)
+		return;
+
+	Enqeue(root);
+
+	while(!isEmpty())
+	{
+		struct Node* current = Pfront();
+		printf("%d ",current->data);
+		if(current->left!=NULL)
+			Enqeue(current->left);
+		if(current->right!=NULL)
+			Enqeue(current->right);
+
+		Pop();
+	}
+}
+
+struct Node* Delete(struct Node* root,int data)
+{
+	if(root==NULL)
+		return root;
+
+	else if(data < root->data)
+		root->left = Delete(root->left,data);
+
+	else if(data > root->data)
+		root->right = Delete(root->right,data);
+
+	else
+	{
+		if(root->left==NULL && root->right==NULL)
+		{
+			free(root);
+			root = NULL;
+		}
+
+		else if(root->left!=NULL && root->right==NULL)
+		{
+			struct Node* temp = root->left;
+			free(temp);
+			root = root->left;
+		}
+		else if(root->right!=NULL&& root->left==NULL)
+		{
+			struct Node* temp = root->right;
+			free(temp);
+			root = root->right;
+		}
+
+		else
+		{
+			struct Node* temp = FindMin(root->right);
+			root->data = temp->data;
+			root->right = Delete(root->right,temp->data); 
+		}
+
+	}
+	return root;
+}
+
+void InOrder(struct Node* root)
+{
+	if(root==NULL)
+		return;
+
+	InOrder(root->left);
+	printf("%d ",root->data);
+	InOrder(root->right);
+}
+
+void PreOrder(struct Node* root)
+{
+	if(root==NULL)
+		return;
+
+	printf("%d ",root->data);
+	PreOrder(root->left);
+	PreOrder(root->right);
+}
+
+struct Node* insert(struct Node* root,int data)
+{
+	if(root==NULL)
+		root =  NewNode(data);
+
+	else if(data <= root->data)
+		root->left = insert(root->left,data);
+	
+	else root->right = insert(root->right,data);
+
+	return root;
+
+}
+
+void traverse(struct Node* root)
+{
+	printf("\n\nPreOrder traversal : "); PreOrder(root);
+	printf("\nInorder traversal  :   "); InOrder(root);
+	printf("\nPostOrder traversal:   "); PostOrder(root);
+	printf("\n Level Order Traversal: ");LevelOrder(root);
+}
+
 int main()
 {
- 	struct bstNode* root;
+	struct Node* root = NULL;
 
- 	int i;
+	root = insert(root,12);
+	root = insert(root,5);
+	root = insert(root,8);
+	root = insert(root,1);
+	root = insert(root,9);
+	root = insert(root,7);
 
- 	root = NULL;
- 	root = insert(root , 15);
- 	root = insert(root , 20);
- 	root = insert(root , 25);
- 	root = insert(root , 8);
- 	root = insert(root , 12);
- 	root = insert(root , 14 );
+	traverse(root);
 
- 	printf("Enter number to be searched :\n");
- 	scanf("%d",&i);
+	Delete(root,8);
 
- 	if(search(root,i))
- 		printf("Found");
+	traverse(root);
 
- 	else
- 		printf("Not Found");
-
- 	return 0;
+	return 0;
 }
