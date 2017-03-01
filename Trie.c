0e4c2aa8c91d7dc90e8529002499548979a1a508
Amit Bansal <007amitbansal@gmail.com>
2017-03-02 00:49:13 +0530

    Trie Insert, Search and delete

--- Trie.c
+++ Trie.c
@@ -1,45 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ALPHABET_SIZE 26
 #define false 0
 #define true 1
-#define ALPHABET_SIZE 26
-#define CHAR_TO_INT(c) (int)c - (int)'a';
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+#define FREE(n)\
+	free(n);	\
+	n = NULL;
 
-// Trie insertion and searching
-
-struct Node
+// Trie insert , search and delete
+struct Node 
 {
+	struct Node *children[ALPHABET_SIZE];
 	int isLeaf;
-	struct Node* children[ALPHABET_SIZE];
 };
-
-struct Node* NewNode()
-{
-	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-
-	if(temp)
-	{
-		temp->isLeaf = false;
-		int i;
-
-		for(i=0;i<ALPHABET_SIZE;i++)
-			temp->children[i] = NULL;
-	}
-
-	return temp;
-}
 
 int search(struct Node* root,const char *key)
 {
-	int index;
 	int level;
 	int length = strlen(key);
+	int index;
 	struct Node* crawl = root;
 
-	for(level = 0;level<length;level++)
+	for(level=0;level<length;level++)
 	{
-		index = CHAR_TO_INT(key[level]);
+		index = CHAR_TO_INDEX(key[level]);
 		if(!crawl->children[index])
 			return false;
 
@@ -49,22 +35,93 @@ int search(struct Node* root,const char *key)
 	return (crawl!=NULL && crawl->isLeaf);
 }
 
+struct Node* NewNode()
+{
+	struct Node* pNode = (struct Node*)malloc(sizeof(struct Node));
+
+	if(pNode)
+	{
+		int i;
+		pNode->isLeaf = false;
+
+		for(i=0;i<ALPHABET_SIZE;i++)
+			pNode->children[i] = NULL;
+
+	}
+	return pNode;
+}
+
 void insert(struct Node* root,const char *key)
 {
-	int index;
-	int length = strlen(key);
 	int level;
+	int length = strlen(key);
+	int index;
+
 	struct Node* crawl = root;
 
-	for(level=0;level<length;level++)
+	for(level=0; level<length; level++)
 	{
-		index = CHAR_TO_INT(key[level]);
+		index = CHAR_TO_INDEX(key[level]);
+
 		if(!crawl->children[index])
 			crawl->children[index] = NewNode();
 
 		crawl = crawl->children[index];
 	}
 	crawl->isLeaf = true;
+}
+
+int isEmptyNode(struct Node* pNode)
+{
+	int i;
+
+	for(i=0;i<ALPHABET_SIZE;i++)
+		if(pNode->children[i])
+			return 0;
+	return 1;
+}
+
+int DeleteKey(struct Node* pNode,const char *key,int level,int len)
+{
+	if(pNode)
+	{
+		if(level==len)
+		{
+			if(pNode->isLeaf)
+			{
+				pNode->isLeaf = false;
+
+				if(isEmptyNode(pNode))
+					return true;
+			}
+			return false;
+		}
+
+		else
+		{
+			int index = CHAR_TO_INDEX(key[level]);
+
+			if(DeleteKey(pNode->children[index],key,level+1,len))
+			{
+				FREE(pNode->children[index]);
+
+				return((pNode->isLeaf==0) && isEmptyNode(pNode));
+			}
+		}
+	}
+
+	return false;
+}
+
+void Delete(struct Node* root,const char *key)
+{
+	int len = strlen(key);
+
+	if(key>0)
+	{
+		DeleteKey(root,key,0,len);
+
+	}
 }
 
 int main()
@@ -81,11 +138,15 @@ int main()
 	for(i=0;i<total;i++)
 		insert(root,strings[i]);
 
-	printf("%s---%s\n","these",output[search(root,"these")]);
+	printf("\n%s---%s\n","these",output[search(root,"these")]);
 	printf("%s---%s\n","the",output[search(root,"the")]);
 	printf("%s---%s\n","that",output[search(root,"that")]);
 	printf("%s---%s\n","this",output[search(root,"this")]);
-	printf("%s---%s\n","bye",output[search(root,"bye")]);
 	
+	Delete(root,"the");
+	printf("\nDeleting \"the\" \n\n");
+
+	printf("%s---%s\n","the",output[search(root,"the")]);
+
 	return 0;
 }
